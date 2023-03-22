ec1fe4ede3440133fc1b7dce9298208c0c82d42a
kgakollo <mathibekgakollo@gmail.com>
2023-03-22 07:18:16 -0700

    5-main.c

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,39 @@
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = (98);
+
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = (0);
+
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = (0xff);
+
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = (-1);
+
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    return (0);
+}

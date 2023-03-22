b62734b9d8545c59b30b21fcb0e524ea5b9984ad
kgakollo <mathibekgakollo@gmail.com>
2023-03-22 03:47:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -Make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet(void
+		)
+{
+	char c:
+	int i = 0;
+
+	while (i <= 0)
+	{f
+	for (c = 'a'; c <= 'z'; c++)
+	}
+	_putchar(c);
+	{
+	_putchar('\n');
+	1++;
+	}
+}

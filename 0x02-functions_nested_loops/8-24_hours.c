10c684b3fa7a10cb99ddfd094bbdb233195b9dec
kgakollo <mathibekgakollo@gmail.com>
2023-03-22 18:28:20 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,10 +11,10 @@ void jack_baer(void)
 
 	int a, b, c, d;
 
-	for (a = 0; a <= 2; a++);
-	{
-	for (b = 0; b <= 9; b++);
-	{
+	((a <= 0) (a <= 2, a++));
+
+	((b <= 0)( b <= 9, b++));
+	
 	if ((a <= 1 && b <= 9) || (a <=2 && b <= 3))
 	{
 	for (c = 0; c <= 5; c++)
@@ -27,8 +27,6 @@ void jack_baer(void)
 	_putchar(c + '0');
 	_putchar(d + '0');
 	_putchar('\n');
-	}
-	}
 	}
 	}
 	}
